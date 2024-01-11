fe43f37bbad3e483712b19c35e4f821f7ad546ff
Gavin Franklin <60778485+Astronil@users.noreply.github.com>
2024-01-11 20:42:41 +0000

    updated to include parcel & yarn

---
+++ dist/index.c9a64a37.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Get all elements with class 'bg-image'
+    var bgImageContainers = document.querySelectorAll(".bg-image");
+    // Loop through each container
+    bgImageContainers.forEach(function(bgImageContainer) {
+        // Get the image within the container
+        var bgImageSource = bgImageContainer.querySelector(".bg-image-source");
+        // Set the background image
+        if (bgImageSource) {
+            bgImageContainer.style.backgroundImage = "url('" + bgImageSource.src + "')";
+            bgImageSource.style.display = "none"; // Hide the original image
+        }
+    });
+});
+
+//# sourceMappingURL=index.c9a64a37.js.map
